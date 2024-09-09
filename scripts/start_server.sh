617c065219be8eb9e1670064eff8f227e549ff61
sidda888 <gcpt5st@gmail.com>
2024-09-09 11:43:35 +0530

    Web-App-Commit

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-# Navigate to the app directory 
-cd /home/ec2-user/node-app
-
-# Start the Node.js application using PM2 (recommended over background process handling)
-pm2 start index.js --name "node-app"
-pm2 save
+systemctl restart httpd
